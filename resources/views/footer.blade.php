a1820ef91bdf490826abebac6160a4377b3db246
Sambath <sambath@rightcomply.com>
2023-04-27 14:22:51 +0530

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="panel panel-default">
+   <!-- <div class="panel-body">
+      Panel content
+    </div> -->
+    <div class="panel-footer">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+</div>
